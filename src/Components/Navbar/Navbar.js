af0412b21f893817d3f42cbe1d2ebcde1004fa8d
Eternalgratis <tyaps002@gmail.com>
2022-08-26 18:27:06 +0100

    Connect API

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,25 @@
+/* eslint-disable */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import userImage from '../../images/user-icon.png';
+import './Navbar.css';
 
 const Navbar = () => (
-  <nav>
-    <h1 className="nav-title">Bookstore CMS</h1>
-    <ul className="nav-list">
-      <NavLink to="/">
-        Books
-      </NavLink>
-      <NavLink to="categories">
-        Categories
-      </NavLink>
-    </ul>
-    <i className="fa-light fa-user" />
+  <nav className="nav-container">
+    <div className="title-list">
+      <h1 className="nav-title">Bookstore CMS</h1>
+      <ul className="nav-list">
+        <NavLink to="/" className="nav-book">
+          Books
+        </NavLink>
+        <NavLink to="categories" className="nav-cat">
+          Categories
+        </NavLink>
+      </ul>
+    </div>
+    <div>
+      <img className="user-image" src={userImage} alt="user-img" />
+    </div>
   </nav>
 );
 export default Navbar;
